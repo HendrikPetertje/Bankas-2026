0f85f8dac8427cd7239bae21580fcc399716548d
Peter van der Meulen <peter@hendrikpeter.net>
2026-04-13 09:16:21 +0200

    Textual fixes

--- src/slides/ForestWelcome/slideContent.ts
+++ src/slides/ForestWelcome/slideContent.ts
@@ -1,16 +1,15 @@
 export const welcomeContent = `**Lägret för dig som vill upptäcka en annan värld.**
+Jesus sa:
+> "Himmelriket är som en skatt som är gömd i en åker."
 
 Det börjar som en vanlig resa.
-Men när du passerar genom byn Vi, Alnö händer något…
+Men när du passerar genom byn Vi på Alnö händer något…
 
 Som om du går genom en osynlig dörr.
 Som om vägen plötsligt leder rakt in i en annan värld.
 
-Jesus sa:
-> "Himmelriket är som en skatt som är gömd i en åker."
-
 På andra sidan väntar Konungens Rike. Vid Bänkåsviken öppnar sig en plats fylld av äventyr, mysterier och berättelser. I skogens skuggor gömmer sig hemligheter, på ängarna reser sig tälten där ni bor tillsammans, och vid stranden väntar lekar, gemenskap och äventyr vid vattnet.
 
-Här möter du både vänner och utmaningar. Här får du höra om Kungen – en kung som inte styr med makt, utan med kärlek. Och kanske upptäcker du att just du har en viktig plats i hans rike.
+Här möter du både vänner och utmaningar. Här får du höra om Kungen, en kung som inte styr med makt, utan med kärlek. Och kanske upptäcker du att just du har en viktig plats i hans rike.
 
 Under "Konungens Rike" fylls dagarna med spårningar, lekar, tävlingar, pyssel, teater, musik, lägerbål och aktiviteter både i skogen och vid stranden. I kiosken finns glass och godis, och varje dag bjuder på skratt, gemenskap och spännande berättelser från Bibeln.`;
